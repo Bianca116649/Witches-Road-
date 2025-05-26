e749a417f3f9a2b3e938a1f0de6321e0139932c5
Grigore <grigorebianca76@gmail.com>
2025-05-26 15:42:56 +0300

    update4

--- src/Spell.cpp
+++ src/Spell.cpp
@@ -4,7 +4,7 @@
 Spell::Spell() : Items(), castTime(0), Sx(0), Sy(0), Sw(0), Sh(0), canHeal(false) {}
 
 Spell::Spell(const float castTime, const int Sx, const int Sy, const int Sw, const int Sh, const bool canHeal)
-        :castTime(castTime), Sx(Sx), Sy(Sy), Sw(Sw), Sh(Sh), canHeal(canHeal){}
+        :Items(), castTime(castTime), Sx(Sx), Sy(Sy), Sw(Sw), Sh(Sh), canHeal(canHeal){}
 
 Spell::Spell(const Spell &other)
   : Items(other),
@@ -19,7 +19,7 @@ Spell& Spell:: operator=(const Spell &other) {
     if (this == &other) {
         return *this;
     }
-    Items::operator =(other);
+    Items::operator=(other);
     castTime = other.castTime;
     Sx = other.Sx;
     Sy = other.Sy;
