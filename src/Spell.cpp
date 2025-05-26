e7566c4f63e2d2a099e3d006ac208d96c050fabe
Grigore <grigorebianca76@gmail.com>
2025-05-26 16:38:24 +0300

    update11

--- src/Spell.cpp
+++ src/Spell.cpp
@@ -2,20 +2,13 @@
 #include "../include/Items.h"
 #include <list>
 
-Spell::Spell() : Items(damage, name), castTime(0), Sx(0), Sy(0), Sw(0), Sh(0), canHeal(false) {
+Spell::Spell() : Items(), castTime(0), Sx(0), Sy(0), Sw(0), Sh(0), canHeal(false) {
 }
 
 Spell::Spell(const float castTime, const int Sx, const int Sy, const int Sw, const int Sh, const bool canHeal)
         :castTime(castTime), Sx(Sx), Sy(Sy), Sw(Sw), Sh(Sh), canHeal(canHeal){}
 
-Spell::Spell(const Spell &other)
-  : Items(other),
-    castTime(other.castTime),
-    Sx(other.Sx),
-    Sy(other.Sy),
-    Sw(other.Sw),
-    Sh(other.Sh),
-    canHeal(other.canHeal){}
+Spell::Spell(const Spell &other)=default;
 
 Spell& Spell:: operator=(const Spell &other) {
     if (this == &other) {
