cb864161c22bcb2dd1a739606f0e15b73d3da391
Grigore <grigorebianca76@gmail.com>
2025-05-26 16:34:46 +0300

    update10

--- src/Spell.cpp
+++ src/Spell.cpp
@@ -2,7 +2,8 @@
 #include "../include/Items.h"
 #include <list>
 
-Spell::Spell() : Items(), castTime(0), Sx(0), Sy(0), Sw(0), Sh(0), canHeal(false) {}
+Spell::Spell() : Items(damage, name), castTime(0), Sx(0), Sy(0), Sw(0), Sh(0), canHeal(false) {
+}
 
 Spell::Spell(const float castTime, const int Sx, const int Sy, const int Sw, const int Sh, const bool canHeal)
         :castTime(castTime), Sx(Sx), Sy(Sy), Sw(Sw), Sh(Sh), canHeal(canHeal){}
