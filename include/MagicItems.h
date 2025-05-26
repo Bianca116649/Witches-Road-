88c3623296c559a9e62558277e91bc48a41650f1
Grigore <grigorebianca76@gmail.com>
2025-05-26 13:36:00 +0300

    update din nou

--- include/MagicItems.h
+++ include/MagicItems.h
@@ -20,6 +20,7 @@ public:
     MagicItems& operator=(const MagicItems &other);
     int levelFromItems() const override;
 
+    using Weapon::giveDamage;
     void giveDamage(const std::list<Ghost*>& ghosts) override;
 };
 
